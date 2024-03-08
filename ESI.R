8537770393d7e8dbbd54bea407045b9da923bd3c
alaruqihwala <alaruqihwala@utexas.edu>
2024-03-07 19:16:59 -0600

    Code with duration and parent code

--- ESI.R
+++ ESI.R
@@ -5,6 +5,36 @@ head(fitbit_interday)
 ncol(fitbit_interday)
 nrow(fitbit_interday)
 colnames(fitbit_interday)
-fitbit_interday %>% select(activities_minuteslightlyactive,	activities_minutesfairlyactive,
-                           activities_minutesveryactive,	activities_activitycalories) %>% summary()
+
+# Doing aggregate the code that works the best 
+fitbit_agger <- fitbit_interday %>%
+  group_by(identifier) %>%
+  mutate(Minutes_Moderatley_Active = activities_minutesfairlyactive + activities_minutesveryactive) %>%
+  summarise(
+    Daily_Steps_Mean = mean(activities_steps),
+    Minutes_Sedentary_Mean = mean(activities_minutessedentary),
+    Minutes_Moderatley_Active_Mean = mean(Minutes_Moderatley_Active)
+  )
+
+# Parent code that has minutes and hours for sedentary to make it more readable 
+fitbit_agger <- fitbit_interday %>%
+  group_by(identifier) %>%
+  mutate(Minutes_Moderatley_Active = activities_minutesfairlyactive + activities_minutesveryactive) %>%
+  summarise(
+    Daily_Steps_Mean = mean(activities_steps),
+    Minutes_Sedentary_Mean = mean(activities_minutessedentary),
+    Minutes_Moderatley_Active_Mean = mean(Minutes_Moderatley_Active), Hours_Sedentary = mean(activities_minutessedentary) / 60 
+  )
+
+# Duration parent code 
+fitbit_agger <- fitbit_interday %>%
+  group_by(identifier) %>%
+  summarise(
+    Days_Collected = n_distinct(as.Date(date)),
+    Daily_Steps_Mean = mean(activities_steps),
+    Minutes_Sedentary_Mean = mean(activities_minutessedentary),
+    Hours_Sedentary_Mean = mean(activities_minutessedentary / 60),
+    Minutes_Moderatley_Active_Mean = mean(activities_minutesfairlyactive + activities_minutesveryactive)
+  )
+
 
