4c91df45cd0c67428582d674ade54616dc32bdf9
alaruqihwala <alaruqihwala@utexas.edu>
2024-03-27 17:51:10 -0500

    Updated script

--- ESI.R
+++ ESI.R
@@ -36,5 +36,35 @@ fitbit_agger <- fitbit_interday %>%
     Hours_Sedentary_Mean = mean(activities_minutessedentary / 60),
     Minutes_Moderatley_Active_Mean = mean(activities_minutesfairlyactive + activities_minutesveryactive)
   )
+# Doing the same code, but for ambassador families
+fitbit_amba <- read_csv("/Users/wala/Documents/data2/Fitbit_Interday_Ambassadors.csv")
 
+fitbit_2 <- fitbit_amba %>%
+  group_by(identifier) %>%
+  mutate(Minutes_Moderatley_Active = activities_minutesfairlyactive + activities_minutesveryactive) %>%
+  summarise(
+    Daily_Steps_Mean = mean(activities_steps),
+    Minutes_Sedentary_Mean = mean(activities_minutessedentary),
+    Minutes_Moderatley_Active_Mean = mean(Minutes_Moderatley_Active), Hours_Sedentary = mean(activities_minutessedentary) / 60 
+  )
+  
+# Combining the ambassadors and others together into one df
+combined_data<-rbind(fitbit_2,fitbit_agger)
 
+# Making the data to long format for better analysis 
+
+combined_data_long <- pivot_longer(merged_data, cols = -identifier, names_to = "variable",values_to = "value")
+
+# Adding sex and DOB 
+merged_data <- merge(combined_data, DOB_Gen_032224, by = "identifier")
+
+# DOB and sex in long format
+merged_data_long <- merged_data %>%
+  pivot_longer(
+    cols = c("Daily_Steps_Mean", "Minutes_Sedentary_Mean", "Minutes_Moderatley_Active_Mean", "Hours_Sedentary", "Sex", "Age"),
+    names_to = "Variable",
+    values_to = "Value"
+  )
+
+# View the long format data
+View(merged_data_long)
